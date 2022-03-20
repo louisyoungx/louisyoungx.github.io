6c53571d7554e6ccc31e8b0984a4490e1362fbfd
louisyoungx <louisyoungx@users.noreply.github.com>
2022-03-20 21:49:11 +0000

    deploy: f2b1aa6a68f6d9ad864637bef1cf991db41580a2

---
+++ assets/index.html.b6130824.js
@@ -0,0 +1,1 @@
+const t={key:"v-7f8b9eb4",path:"/tags/technology/",title:"Tags",lang:"en-US",frontmatter:{title:"Tags",layout:"Tags"},excerpt:"",headers:[],git:{},filePathRelative:null};export{t as data};
